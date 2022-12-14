7abba6f229b2ecacd35af1a63ca2d0a6aef77378
Alex Steckler <alexander.steckler@informatik.hs-fulda.de>
2022-12-14 14:47:58 +0100

    feat: add new components

--- RescueMapCreator/src/app/app-routing.module.ts
+++ RescueMapCreator/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CreateEditComponent } from './create-edit/create-edit.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+
+const routes: Routes = [
+{
+  path: 'createEdit', component:CreateEditComponent
+},
+{
+  path: 'home', component:HomeComponent
+},
+{
+  path: 'settings', component:SettingsComponent
+},
+{
+  path: '**',
+  redirectTo: 'home'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- RescueMapCreator/src/app/app.module.ts
+++ RescueMapCreator/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEditComponent } from './create-edit/create-edit.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEditComponent,
+    HomeComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
