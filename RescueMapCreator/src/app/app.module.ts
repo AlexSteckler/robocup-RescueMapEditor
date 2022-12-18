c3b16632829425d79843ed0018bd36b1d05999d4
Julian Werner <info@werner-julian.de>
2022-12-18 02:59:39 +0100

    feat: first tests

--- RescueMapCreator/src/app/app.module.ts
+++ RescueMapCreator/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { CreateEditComponent } from './create-edit/create-edit.component';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { TileComponent } from './create-edit/tile/tile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateEditComponent,
     HomeComponent,
-    SettingsComponent
+    SettingsComponent,
+    TileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- RescueMapCreator/src/app/create-edit/create-edit.component.ts
+++ RescueMapCreator/src/app/create-edit/create-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CdkDragDrop, transferArrayItem} from "@angular/cdk/drag-drop";
+import {Tile} from "./tile/dto/tile.dto";
 
 @Component({
   selector: 'app-create-edit',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-edit.component.scss']
 })
 export class CreateEditComponent {
+  tiles = [
+    {number: 1},
+    {number: 2},
+    {number: 3},
+    {number: 4},
+    {number: 5},
+    {number: 6},
+    {number: 7},
+    {number: 8},
+    {number: 9},
+  ];
+  grid: (Tile | undefined)[] = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
 
+  drop($event: CdkDragDrop<Tile[]>, tileNumber: number) {
+    //console.log($event.previousContainer.data[$event.previousIndex]);
+    this.grid[tileNumber] = $event.previousContainer.data[$event.previousIndex]
+  }
 }
