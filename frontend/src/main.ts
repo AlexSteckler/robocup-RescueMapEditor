3cd456965240ee870122cc88ba5acc128a65ce2e
Julian Werner <info@werner-julian.de>
2023-01-23 16:44:45 +0100

    test: isDev

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,6 +7,7 @@ import {environment} from "./environments/environment";
 if (environment.production) {
   enableProdMode();
 }
+console.log(isDevMode());
 
 if (!isDevMode()) {
   window.console.log = () => {
