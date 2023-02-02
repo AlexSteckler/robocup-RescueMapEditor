92d19e1844fe4a391f9ff3b7aa4b6a88250ad2af
Julian Werner <info@werner-julian.de>
2023-02-02 01:12:39 +0100

    fix: change url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   kUrl: 'https://keycloak.crashtec.de/auth',
   kRealm: 'RoboCup',
   kClientId: 'app-frontend-mapeditor',
-  baseUrlV1: 'https://mapeditor.steckler-alex.de/api/v1',
+  baseUrlV1: 'https://mapeditor.robocupjunior.de/api/v1',
   local: false
 };
