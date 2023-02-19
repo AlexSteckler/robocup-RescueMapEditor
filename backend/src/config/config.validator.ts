975e4a5c9a4eca316924f032ed940f912af2cc6e
Julian Werner <info@werner-julian.de>
2023-02-19 22:51:43 +0100

    chore: change env

--- backend/src/config/config.validator.ts
+++ backend/src/config/config.validator.ts
@@ -2,13 +2,13 @@ import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
   PORT: Joi.number().default(3002),
-  MONGO_URI: Joi.string().default('mongodb://mongoDB:27017/robocup-mapeditor'),
+  MONGO_URI: Joi.string().default('mongodb-usertest://mongoDB:27017/robocup-mapeditor'),
   KEYCLOAK_URI: Joi.string().default('https://keycloak.crashtec.de/auth'),
   KEYCLOAK_REALM: Joi.string().default('RoboCup'),
   KEYCLOAK_CLIENT_ID: Joi.string().default('app-backend-mapeditor'),
   KEYCLOAK_SECRET: Joi.string().default('secret'),
   KEYCLOAK_USER: Joi.string().default('admin'),
   KEYCLOAK_PASSWORD: Joi.string().default('KeycloakPassword'),
-  URI: Joi.string().default('https://mapeditor.robocupjunior.de'),
+  URI: Joi.string().default('https://mapeditor.steckler-alex.de'),
   LOCAL: Joi.boolean().default(false),
 });
